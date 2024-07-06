e5ee3bcdd9594c4f1d4d7e196e82db8596449a58
Michael Bestas <mkbestas@lineageos.org>
2024-07-07 05:00:30 +0800

    lavender: props: Use AOSP default Codec2/OMX ranks
    Change-Id: Idf55ec02c2c0f4d2fa73f949cffef0d820a54570
    Signed-off-by: Sã Śâjjãd <mdsojola12@gmail.com>

--- device.mk
+++ device.mk
@@ -273,9 +273,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
-PRODUCT_COPY_FILES += \
-     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
-
 # Netd
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
