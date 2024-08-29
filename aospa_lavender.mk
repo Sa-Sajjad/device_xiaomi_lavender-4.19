c3de792bcd30b94dee3c40b64240d132acdf8178
Sa Sajjad <sqsa93103@gmail.com>
2024-08-29 22:22:48 +0600

    lav fix
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- aospa_lavender.mk
+++ aospa_lavender.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common aospaOS stuff
-$(call inherit-product, vendor/aospa/config/common_full_phone.mk)
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
 
 # Inherit from lavender device
 $(call inherit-product, device/xiaomi/lavender/device.mk)
