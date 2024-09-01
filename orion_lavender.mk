cdd1d22e1f55c59a73e23f1871fb51a316d2551a
Sa Sajjad <sqsa93103@gmail.com>
2024-09-01 21:13:01 +0600

    lavender: Initial OrionOS Bringup
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- orion_lavender.mk
+++ orion_lavender.mk
@@ -8,23 +8,29 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff
+# Inherit some common OrionOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-PRODUCT_NAME := lineage_lavender
+# OrionOS stuff
+ORION_MAINTAINER := Sajjad
+ORION_MAINTAINER_LINK := https://t.me/sa_sajjadx
+TARGET_ENABLE_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Common flags
+# ORION_GAPPS := true
+# BUILD_GOOGLE_CONTACTS := true
+# BUILD_GOOGLE_DIALER := true
+# BUILD_GOOGLE_MESSAGE := true
+
+PRODUCT_NAME := orion_lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
 TARGET_VENDOR_PRODUCT_NAME := lavender
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V12.5.3.0.QFGCNXM release-keys"
-
-BUILD_FINGERPRINT := xiaomi/lavender/lavender:10/QKQ1.190910.002/V12.5.3.0.QFGCNXM:user/release-keys
