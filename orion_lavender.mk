a7d83c9374ce4f649d0a7cbb94d06e4b5f44bc95
Sã Śâjjãd <mdsojola12@gmail.com>
2025-02-18 20:08:10 +0600

    lavender: Initial OrionOS Bringup
    Signed-off-by: Sã Śâjjãd <mdsojola12@gmail.com>

--- orion_lavender.mk
+++ orion_lavender.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff
+# Inherit some common orionOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from lavender device
@@ -17,7 +17,12 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 TARGET_ENABLE_BLUR := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_lavender
+# ORION_MAINTAINER := Sã Śâjjãd
+ORION_MAINTAINER_LINK := https://t.me/sa_sajjadx
+ORION_BUILD_TYPE := Official
+# ORION_GAPPS := true
+
+PRODUCT_NAME := orion_lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
